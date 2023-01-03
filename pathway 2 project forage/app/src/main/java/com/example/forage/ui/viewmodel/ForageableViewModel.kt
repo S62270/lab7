d2450b1aa5d22c7215e03fdccbc951af4ec14d81
S62270 <s62270@ocean.umt.edu.my>
2023-01-03 21:28:42 +0800

    initial commit

--- pathway 2 project forage/app/src/main/java/com/example/forage/ui/viewmodel/ForageableViewModel.kt
+++ pathway 2 project forage/app/src/main/java/com/example/forage/ui/viewmodel/ForageableViewModel.kt
@@ -17,6 +17,7 @@ package com.example.forage.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.forage.data.ForageableDao
 import com.example.forage.model.Forageable
@@ -30,13 +31,13 @@ import kotlinx.coroutines.launch
 
 // TODO: pass a ForageableDao value as a parameter to the view model constructor
 class ForageableViewModel(
-    // Pass dao here
-): ViewModel() {
+    private val forageableDao: ForageableDao):ViewModel() {
 
     // TODO: create a property to set to a list of all forageables from the DAO
-
+    val forageables = forageableDao.getForageables().asLiveData()
     // TODO : create method that takes id: Long as a parameter and retrieve a Forageable from the
     //  database by id via the DAO.
+    fun getForageable(id: Long) = forageableDao.getForageable(id).asLiveData()
 
     fun addForageable(
         name: String,
@@ -51,39 +52,54 @@ class ForageableViewModel(
             notes = notes
         )
 
-    // TODO: launch a coroutine and call the DAO method to add a Forageable to the database within it
+        // TODO: launch a coroutine and call the DAO method to add a Forageable to the database within it
+            viewModelScope.launch(Dispatchers.IO) {
+                forageableDao.insert(forageable)
+            }
+        }
 
-    }
+        fun updateForageable(
+            id: Long,
+            name: String,
+            address: String,
+            inSeason: Boolean,
+            notes: String
+        ) {
+            val forageable = Forageable(
+                id = id,
+                name = name,
+                address = address,
+                inSeason = inSeason,
+                notes = notes
+            )
+            viewModelScope.launch(Dispatchers.IO) {
+                // TODO: call the DAO method to update a forageable to the database here
+                forageableDao.update(forageable)
+            }
+        }
 
-    fun updateForageable(
-        id: Long,
-        name: String,
-        address: String,
-        inSeason: Boolean,
-        notes: String
-    ) {
-        val forageable = Forageable(
-            id = id,
-            name = name,
-            address = address,
-            inSeason = inSeason,
-            notes = notes
-        )
-        viewModelScope.launch(Dispatchers.IO) {
-            // TODO: call the DAO method to update a forageable to the database here
+        fun deleteForageable(forageable: Forageable) {
+            viewModelScope.launch(Dispatchers.IO) {
+                // TODO: call the DAO method to delete a forageable to the database here
+                forageableDao.delete(forageable)
+            }
+        }
+
+        fun isValidEntry(name: String, address: String): Boolean {
+            return name.isNotBlank() && address.isNotBlank()
         }
     }
 
-    fun deleteForageable(forageable: Forageable) {
-        viewModelScope.launch(Dispatchers.IO) {
-            // TODO: call the DAO method to delete a forageable to the database here
+    // TODO: create a view model factory that takes a ForageableDao as a property and
+    //  creates a ForageableViewModel
+    class ForageableViewModelFactory(private val ForageableDao: ForageableDao) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ForageableViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return ForageableViewModel(ForageableDao) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
         }
     }
 
-    fun isValidEntry(name: String, address: String): Boolean {
-        return name.isNotBlank() && address.isNotBlank()
-    }
-}
-
-// TODO: create a view model factory that takes a ForageableDao as a property and
-//  creates a ForageableViewModel
